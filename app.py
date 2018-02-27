17ec3a733a3a48cb5bbbb6e32144e7a2cff57b3a
mjkrause <michael.j.krause@gmail.com>
2018-02-26 20:34:26 -0800

    changed app.py - lambda works with curl

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-from chalice import Chalice
+import logging
+import os
+import zipfile
+import io
+# import pandas as pd
+from chalice import Chalice, Response
+from chalicelib.utils import ManifestIO
 
 app = Chalice(app_name='bagit-firecloud-lambda')
+app.debug = True
+app.log.setLevel(logging.DEBUG)
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/exportBag',
+           methods=['POST'],
+           content_types=['application/octet-stream'])
+def exportBag():
+    req_body = app.current_request.raw_body
+    req_query_params = app.current_request.query_params
+    req_headers = app.current_request.headers
+    os.chdir('/tmp')
+    with zipfile.ZipFile(io.BytesIO(req_body), 'r') as archive:
+        archive.extractall()
+    tsv_fname = '/tmp/manifest_bag/data/manifest.tsv'
+    workspace = req_query_params['workspace']
+    namespace = req_query_params['namespace']
+    url = 'https://api.firecloud.org/api/workspaces'
+    # token = os.getenv("TOKEN", None)
+    token = req_headers['Authorization']
+    manifest_io = ManifestIO(tsv_fname, url, workspace, namespace, token)
+    statusCode = manifest_io.import_tsv_to_fc()
+    # tf = os.path.isfile(upload1)
+    # return Response(body={'status': tf},
+    return Response(body=statusCode,
+                    status_code=200,
+                    headers={'Content-Type': 'application/json',
+                             'Accept': 'application/json'})
